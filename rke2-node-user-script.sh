453f6404a4b0a3c52b852ed7047c3f5f829c9c60
blackwood821 <36967614+blackwood821@users.noreply.github.com>
2024-09-19 16:36:34 -0500

    NFSv4 client config changes for RKE2 node user-script
    Switched away from relying on changing the UID of the _pkgsrc user on the Triton volume (NFS server) and instead create the _pkgsrc user with UID 999 on the NFS client and use "local" for the NFSv4 domain. This is simpler since Triton volumes are less configurable.

--- rke2-node-user-script.sh
+++ rke2-node-user-script.sh
@@ -23,24 +23,42 @@ configure_nfs_client() {
 
 	# Don't configure the node for NFSv4 domain mapping because that requires
 	# that the NFS client (this node) and server (Triton volume) have the same
-	# users (e.g. postgres). I found it simpler to change the UID of the _pkgsrc
-	# user on Triton volumes from 999 to 9999.
+	# users (e.g. postgres). I found it simpler to change the UID of the "_pkgsrc"
+	# user on Triton volumes from 999 to 9999 to bypass id mapping altogether.
 	# https://kubernetes.slack.com/archives/C09NXKJKA/p1726770810576419?thread_ts=1726587222.844109&cid=C09NXKJKA
 	# https://discord.com/channels/979453320085250108/979453444727406602/1286412566083665931
+
+	# Take 2... since Triton volumes are not configurable at creation time and more difficult
+	# to configure after creation it may be simpler to create a "_pkgsrc" user with UID 999
+	# on the client. However, this does require either setting the NFSv4 domain on the client
+	# to "local" and leaving the domain on the NFS server unset or setting both to the same domain.
 
 	# use a dummy NFSv4 domain
 	# this doesn't need to be resolvable, it just needs to match on the NFS client and server
 	# NFSV4_DOMAIN="example.local"
-	# IDMAPD_CONF="/etc/idmapd.conf"
+	# Using the domain "local" tells the NFS client to treat user and group names with no domain
+	# as if they belong to the local domain. This allows id mapping to work even when the NFS
+	# server does not have a NFSv4 domain set.
+	NFSV4_DOMAIN=$(mdata-get nfsv4_domain 2>/dev/null || echo "local")
+	IDMAPD_CONF="/etc/idmapd.conf"
 
-	# # check if the Domain line exists in the idmap config file
-	# if grep -q "^Domain" "${IDMAPD_CONF}"; then
-	# 	# if it exists, replace the existing Domain value
-	# 	sed -i "s/^Domain=.*/Domain=${NFSV4_DOMAIN}/" "$IDMAPD_CONF"
-	# else
-	# 	# if it doesn't exist, add the Domain under the [General] section
-	# 	sed -i "/^\[General\]/a Domain=${NFSV4_DOMAIN}" "$IDMAPD_CONF"
-	# fi
+	# check if the Domain line exists in the idmap config file
+	if grep -q "^Domain" "${IDMAPD_CONF}"; then
+		# if it exists, replace the existing Domain value
+		sed -i "s/^Domain\s*=.*/Domain = ${NFSV4_DOMAIN}/" "${IDMAPD_CONF}"
+	else
+		# if it doesn't exist, add the Domain under the [General] section
+		sed -i "/^\[General\]/a Domain = ${NFSV4_DOMAIN}" "${IDMAPD_CONF}"
+	fi
+
+	if id _pkgsrc &>/dev/null; then
+		echo "User '_pkgsrc' already exists"
+	else
+		# the UID needs to be 999 to match the UID of the postgres user in the postgres docker image
+		# and to match the UID of the _pkgsrc user on the Triton volume since we're not configuring
+		# the Triton volume with a NFSv4 domain that matches the domain on the client.
+		useradd -u 999 _pkgsrc
+	fi
 }
 
 main "$@"
